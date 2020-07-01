b3349cf0c1abb1c08514589542f4d1ec5cbdc716
Young <beyoung@amazon.com>
2020-07-01 15:17:18 +0000

    modify build and push

--- yolo/7-yolo-sm-train/build-local.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-region=$(aws configure get region)
-$(aws ecr get-login --registry-ids 763104351884 --region ${region} --no-include-email)
-base_img='763104351884.dkr.ecr.'$region'.amazonaws.com/tensorflow-training:1.15.2-gpu-py36-cu100-ubuntu18.04'
-echo 'base_img:'$base_img
-cd container
-docker build -t yolov4 -f Dockerfile --build-arg BASE_IMG=$base_img .
