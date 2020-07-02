53153531d15431fe500da99e3e122b5651f98f2f
EC2 Default User <ec2-user@ip-172-16-133-38.ec2.internal>
2020-07-02 08:20:01 +0000

    refactor and bug fixing

--- yolo/4-Yolo-SageMaker-Container/container/train.py
+++ yolo/4-Yolo-SageMaker-Container/container/train.py
@@ -2,37 +2,41 @@
 import sys
 import os  
 import shutil
+import subprocess
 
-#bashCommand_template = "darknet detector train /opt/ml/input/data/cfg/coco.data /opt/ml/input/data/cfg/yolov4.cfg /opt/ml/input/data/yolo_model/csdarknet53-omega_final.weights -dont_show" 
-bashCommand_template = "darknet detector train {} {} {}  -dont_show" 
-predict_bashCommand_template = "darknet detector test {} {} {} {} -dont_show" 
+train_bash_command_template = "darknet detector train {} {} {}  -dont_show" 
+predict_bash_command_template = "darknet detector test {} {} {} {} -dont_show" 
 
 def train():
     coco_cfg_sg_path = "/opt/ml/input/data/dinfo/obj.data"
     yolov4_cfg_sg_path = "/opt/ml/input/data/cfg/custom-yolov4-detector.cfg" 
-    #weight_sg_path = "/opt/ml/input/data/yolo_model/csdarknet53-omega_final.weights"
     weight_sg_path = "/opt/ml/input/data/yolo_model/yolov4.conv.137"
     train_local(coco_cfg_sg_path, yolov4_cfg_sg_path, weight_sg_path)
-    if os.path.isfile('backup/yolov4_final.weights'):
-        shutil.copyfile('backup/yolov4_final.weights', '/opt/ml/model/yolov4_final.weights')
+    if os.path.isfile('backup/custom-yolov4-detector_final.weights'):
+        shutil.copyfile('backup/custom-yolov4-detector_final.weights', '/opt/ml/model/custom-yolov4-detector_final.weights')
 
-def train_local(coco_cfg, yolov4_cfg, weights):
-    
-    os.mkdir('backup')
-    bashCommand = bashCommand_template.format(coco_cfg, yolov4_cfg, weights)
-    import subprocess
-    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+def mk_dir(sub_dir, working_dir=None):
+    output_path = None 
+    if working_dir: 
+        output_path = working_dir+'/'+sub_dir
+    else: 
+        output_path = sub_dir 
+    if not os.path.exists(output_path):
+        os.mkdir(output_path) 
+
+def train_local(coco_cfg, yolov4_cfg, weights, working_dir=None):
+    mk_dir('backup', working_dir)
+    train_bash_command = train_bash_command_template.format(coco_cfg, yolov4_cfg, weights)
+    process = subprocess.Popen(train_bash_command.split(), stdout=subprocess.PIPE)
     output, error = process.communicate()
     print(output, error)
    
 
 def predict_local(coco_cfg, yolov4_cfg, weights, test_file, working_dir):
-    
-    os.mkdir('backup')
-    bashCommand = predict_bashCommand_template.format(coco_cfg, yolov4_cfg, weights, test_file)
-    import subprocess
-    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+    bash_command = predict_bash_command_template.format(coco_cfg, yolov4_cfg, weights, test_file)
+    process = subprocess.Popen(bash_command.split(), stdout=subprocess.PIPE)
     output, error = process.communicate()
+    
     if os.path.isfile('predictions.jpg'):
         shutil.copyfile('predictions.jpg', working_dir+'/predictions.jpg')
     print(output, error)
@@ -45,7 +49,8 @@ if __name__ == "__main__":
         coco_cfg = sys.argv[2]   
         yolov4_cfg = sys.argv[3]
         weight = sys.argv[4]
-        train_local(coco_cfg, yolov4_cfg, weight)
+        working_dir = sys.argv[5]
+        train_local(coco_cfg, yolov4_cfg, weight, working_dir)
     elif (sys.argv[1]=="predict_local"):
         coco_cfg = sys.argv[2]   
         yolov4_cfg = sys.argv[3]
@@ -54,7 +59,7 @@ if __name__ == "__main__":
         working_dir = sys.argv[6]
         predict_local(coco_cfg, yolov4_cfg, weight, test_file, working_dir)
     else:
-        print("Missing required argument 'train'.", file=sys.stderr)
+        print("Missing required argument 'train, traing_local or predict_local'.", file=sys.stderr)
         sys.exit(1)
 
 
