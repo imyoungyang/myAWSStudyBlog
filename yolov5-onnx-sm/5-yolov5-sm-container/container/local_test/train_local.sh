3a1cc3deb7caa5dd0c9440314d9dbbe72833296e
EC2 Default User <ec2-user@ip-172-16-29-2.us-west-2.compute.internal>
2020-07-24 15:17:18 +0000

    fix local train docker shm size

--- yolov5-onnx-sm/5-yolov5-sm-container/container/local_test/train_local.sh
+++ yolov5-onnx-sm/5-yolov5-sm-container/container/local_test/train_local.sh
@@ -8,5 +8,5 @@ mkdir -p test_dir/output
 rm test_dir/model/*
 rm test_dir/output/*
 
-# nvidia-docker run -v $(pwd)/test_dir:/opt/ml --rm ${image} train
-docker run -v $(pwd)/test_dir:/opt/ml --rm ${image} train
+nvidia-docker run -v $(pwd)/test_dir:/opt/ml --shm-size=8g --rm ${image} train
+# docker run -v $(pwd)/test_dir:/opt/ml --shm-size=8g --rm ${image} train
